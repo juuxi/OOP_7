4b64d880978f9c13fd95fdf340657c76f2c93de7
Dmitry Shopyrev <dimashopyrev@mail.ru>
2024-11-26 12:39:57 +0300

    interface markup

--- interface.cpp
+++ interface.cpp
@@ -3,6 +3,28 @@
 TInterface::TInterface(QWidget *parent)
     : QWidget(parent)
 {
+    setWindowTitle("Практика №7");
+    setFixedSize(800, 500);
+    power = new QLabel("Точность вычисления", this);
+    power->setGeometry(10, 10, 200, 30);
+
+    power_input = new QLineEdit(this);
+    power_input->setGeometry(210, 15, 30, 20);
+
+    func = new QLabel("Вычислить функцию", this);
+    func->setGeometry(10, 60, 200, 30);
+
+    sin = new QRadioButton("sin", this);
+    sin->setGeometry(210, 60, 70, 30);
+
+    integral_sin = new QRadioButton("Si", this);
+    integral_sin->setGeometry(280, 60, 50, 30);
+
+    submit = new QPushButton("Применить", this);
+    submit->setGeometry(100, 150, 150, 30);
+
+    output = new QLabel(this);
+    output->setGeometry(10, 200, 500, 30);
 }
 
 TInterface::~TInterface()
--- interface.h
+++ interface.h
@@ -2,12 +2,22 @@
 #define TINTERFACE_H
 
 #include <QWidget>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QRadioButton>
 #include "complex.h"
 #include "function.h"
 
 class TInterface : public QWidget
 {
     Q_OBJECT
+    QLabel* power;
+    QLineEdit* power_input;
+    QLabel* func;
+    QRadioButton* sin, *integral_sin;
+    QPushButton* submit;
+    QLabel* output;
 
 public:
     TInterface(QWidget *parent = nullptr);
